ab4e315902fc9772399e066e76bcf9657e8cc04a
Jay <jay.wang.scorpion@gmail.com>
2016-04-14 14:25:08 +0800

    ui-sref

--- app/app/js/main.js
+++ app/app/js/main.js
@@ -5,19 +5,20 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
 $urlRouterProvider.otherwise("/book");
 // Now set up the states
 $stateProvider
-  .state('book_list', {
+  .state('book', {
     url: "/book",
     views:{
       header:{templateUrl:'view/header.html'},
       container:{
         templateUrl:'view/bookList.html',
-        controller:'bookController'
+        controller:'bookController',
+        reloadOnSearch: false
       },
       footer:{templateUrl:'view/footer.html'}
     }
   })
-  .state('book_detail', {
-    url: "/book/:id",
+  .state('bookDetail', {
+    url: "/books/:id",
     views:{
       header:{templateUrl:'view/headerBack.html'},
       container:{
@@ -32,7 +33,7 @@ $stateProvider
     views:{
       header:{templateUrl:'view/header.html'},
       container:{
-        templateUrl:'movice/container.html',
+        templateUrl:'view/moviceList.html',
         controller:'moviceController'
       },
       footer:{templateUrl:'view/footer.html'}
@@ -43,12 +44,13 @@ $stateProvider
     views:{
       header:{templateUrl:''},
       container:{
-        templateUrl:'music/container.html',
+        templateUrl:'view/musicList.html',
         controller:'musicController'
       },
       footer:{templateUrl:'view/footer.html'}
     }
   });
+
 });
 
 // Constant
@@ -61,12 +63,26 @@ myApp.constant('Config', {
   movie_search_id: 'https://api.douban.com/v2/movie/subject/'
 });
 
+myApp.run(function($rootScope, $state, $stateParams) {
+            $rootScope.$state = $state;
+            $rootScope.$stateParams = $stateParams;
+            $rootScope.$on("$stateChangeSuccess",  function(event, toState, toParams, fromState, fromParams) {
+                // to be used for back button //won't work when page is reloaded.
+                $rootScope.previousState_name = fromState.name;
+                $rootScope.previousState_params = fromParams;
+            });
+            //back button function called from back button's ng-click="back()"
+            $rootScope.back = function() {
+                $state.go($rootScope.previousState_name,$rootScope.previousState_params);
+            };
+        });
+
 // book controller
 myApp.controller('bookController',function($rootScope,$scope,Config,$http){
   console.log('bookController');
   console.log(Config.book_search);
   var keywords = 'angular';
-  $http.jsonp(Config.book_search + '?callback=searchBookList&count=10&q=' + keywords);
+  //$http.jsonp(Config.book_search + '?callback=searchBookList&count=10&q=' + keywords);
   window.searchBookList = function(data){
         console.log(data);
         if(data.books.length){
@@ -86,8 +102,8 @@ myApp.controller('bookController',function($rootScope,$scope,Config,$http){
           };
           list.push(item);
         }
-        $scope.bookList = list;
-        console.log($scope.bookList);
+        $rootScope.bookList = list;
+        console.log($rootScope.bookList);
   };
 
   $scope.search = function(){
